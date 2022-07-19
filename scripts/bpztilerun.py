effedaa7b90fa7b146f01b9560f6362dbaff4c9c
Raul Basilides Gomez Del Estal Teixeira <raulteixeira@midway2-login2.rcc.local>
2022-07-19 17:13:10 -0500

    bpzrun

---
+++ scripts/bpztilerun.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import h5py
+from astropy.utils.data import download_file  #import file from URL
+import astropy.table
+import subprocess as sp
+import glob
+import sys
+
+tile = 'DES1155-3749'
+
+list_lines = ['COLUMNS     /home/raulteixeira/photoz/code/software/DESC_BPZ/tests/CosmicShearPZ.columns\n'
+              , f'OUTPUT\t    /home/raulteixeira/scratch-midway2/CosmicShearData/bpztiles/output/pzs/pz_sgY3_{tile}_finer_test.h5\n'
+              , 'SPECTRA     CWWSB4.list\n', 'PRIOR\t  hdfn_gen\n', 'DZ          0.01\n', 'ZMIN        0.005\n'
+              , 'ZMAX        3.505\n', 'MAG         yes\n', 'NEW_AB      no\n', 'MADAU\t    no #TURN OFF MADAU!!!!\n'
+              , 'EXCLUDE     none\n', 'CHECK       yes\n', '#ZC          1.0,2.0\n', '#FC          0.2,0.4\n'
+              , 'VERBOSE     no\n', '#INTERP      0\n', 'ODDS        0.68\n'
+              , f'PROBS      /home/raulteixeira/scratch-midway2/CosmicShearData/bpztiles/output/probs/PZ_OUTPUT_sgY3_probs_{tile}_finer_test\n'
+              , f'PROBS2     no\n'
+              , f'PROBS_LITE no\n', 'GET_Z       yes\n', 'INTERACTIVE yes\n', 'PLOTS       no\n'
+              , 'SAMPLING yes\n', 'NSAMPLES 1\n'
+              , '#ONLY_TYPE   yes\n']
+pars = open('/home/raulteixeira/photoz/code/software/DESC_BPZ/tests/DELVEdata_example.pars', mode='w')
+pars.writelines(list_lines)
+pars.close()
+
+print("pars.close()")
+
+catalog_name = f'/home/raulteixeira/scratch-midway2/CosmicShearData/bpztiles/pzinput/pzinput_sgY3_{tile}.h5'
+#running bpz using subprocess
+sp.run(['python', '/home/raulteixeira/repos/DESC_BPZ/scripts/bpz.py', catalog_name, '-P', '/home/raulteixeira/photoz/code/software/DESC_BPZ/tests/DELVEdata_example.pars'], capture_output=False)
+
+print("end")
