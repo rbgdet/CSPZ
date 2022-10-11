ada59fc313ca5e573bb082d9284269a1a48249f7
Raul Basilides Gomez Del Estal Teixeira <raulteixeira@midway2-login2.rcc.local>
2022-10-11 12:55:29 -0500

    fixed bash scripts

--- scripts/submit_mcal_batch.sh
+++ scripts/submit_mcal_batch.sh
@@ -10,12 +10,12 @@ mkdir tile_${i}
 cd tile_${i}
 cp /home/raulteixeira/repos/CSPZ/scripts/download_tile.py ./.
 cp /home/raulteixeira/repos/CSPZ/scripts/bpztilerun_template.py ./. #
-cp /home/raulteixeira/repos/shearcat/code/measurement/tile_DR3_1_1.csv ./.
+cp /home/raulteixeira/repos/CSPZ/scripts/tile_DR3_1_1.csv ./.
 
 python download_tile.py ${i}
 
 echo "#!/bin/sh
-#SBATCH -t 00:30:00
+#SBATCH -t 00:05:00
 #SBATCH --partition=chihway
 #SBATCH --account=pi-chihway
 #SBATCH --job-name=BPZ_${i}
@@ -25,12 +25,12 @@ echo "#!/bin/sh
 python bpztilerun_template.py --tilename ${i}\
 			  --spectra CWWSB4.list\
  			  --prior hdfn_gen\
- 			  --OutPath /scratch/midway2/raulteixeira/CosmicShearData/tile_${i}/pzout_${i}.h5 
-
+ 			  --OutPath /scratch/midway2/raulteixeira/CosmicShearData/tile_${i}/pzout_${i}.h5
+\
 rm -rf /scratch/midway2/raulteixeira/CosmicShearData/tile_${i}/decade.ncsa.illinois.edu
 rm /scratch/midway2/raulteixeira/CosmicShearData/tile_${i}/*py
-rm /scratch/midway2/raulteixiera/CosmicShearData/tile_${i}/tile_DR3_1_1.csv
-rm /scratch/midway2/raulteixeira/CosmicShearData/tile_${i}/table.h5
+rm /scratch/midway2/raulteixeira/CosmicShearData/tile_${i}/tile_DR3_1_1.csv
+#rm /scratch/midway2/raulteixeira/CosmicShearData/tile_${i}/table.h5
 
 ">submit
 
